56a396d30d80dd2bc417d78cd59f46970a002891
RakhmatilloRR <rrr.engineer.94@gmail.com>
2022-06-05 14:33:14 +0500

    first message

--- ArrayMethods/0link.js
+++ ArrayMethods/0link.js
@@ -2,3 +2,36 @@
 // ---> https://www.digitalocean.com/community/tutorials/how-to-implement-javascript-array-methods-from-scratch#introduction
 // ! 02. All Methods;
 // ---> https://blog.logrocket.com/javascript-array-methods/
+const arr = [1, 2, 3, 4, 5];
+// Iterating over collections
+arr.forEach();
+arr.map();
+arr.filter();
+arr.reduce();
+// searching
+arr.findIndex();
+arr.find();
+arr.indexOf();
+arr.lastIndexOf();
+arr.every();
+arr.some();
+arr.includes();
+// flattening
+arr.flat();
+arr.flatMap();
+// Joining, appending, and reversing arrays
+arr.concat();
+arr.join();
+arr.reverse();
+// Adding, removing, and appending values
+arr.shift();
+arr.unshift();
+arr.slice();
+arr.splice();
+arr.pop();
+arr.push();
+arr.fill();
+// With generators
+arr.values();
+arr.keys();
+arr.entries();
