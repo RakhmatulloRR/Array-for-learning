2687d4f99ece609c5b9305a6e60201e02a6c38e7
RakhmatilloRR <rrr.engineer.94@gmail.com>
2022-07-05 21:38:55 +0500

    1773. Count Items Matching a Rule

--- solved/createTargetArray.js
+++ solved/createTargetArray.js
@@ -1,1 +1,29 @@
-createTargetArray
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+// var createTargetArray = function (nums, index) {
+//   let target = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     if (target[index[i]] || target[index[i]] === 0) {
+//       let left = target.slice(0, index[i]);
+//       left.push(nums[i]);
+//       let right = target.slice(index[i]);
+//       target = left.concat(right);
+//     } else {
+//       target[index[i]] = nums[i];
+//     }
+//   }
+//   return target;
+// };
+var createTargetArray = function (nums, index) {
+  let target = [];
+  for (let i = 0; i < nums.length; i++) {
+    target.splice(index[i], 0, nums[i]);
+  }
+  return target;
+};
+let nums = [0, 1, 0],
+  index = [0, 1, 0];
+console.log(createTargetArray(nums, index)); // [0,4,1,3,2]
