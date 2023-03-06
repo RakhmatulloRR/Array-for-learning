431aa0ea9e91d3f22fff223ca38e15c0808a038b
RakhmatilloRR <rrr.engineer.94@gmail.com>
2023-03-06 11:22:00 +0500

    array.find() method was reviewed

--- Array/ArrayMethods/filter.js
+++ Array/ArrayMethods/filter.js
@@ -1,15 +1,14 @@
-// Array.prototype.filter1 = function (callback) {
-//   const newArray = [];
-//   for (let i = 0; i < this.length; i++) {
-//     if (callback(this[i], i, this)) {
-//       newArray.push(this[i]);
-//     }
-//   }
-//   return newArray;
-// };
-// // !========== Test ===========
-// const arr = [2, 3, 4, 5, 6, 7, 8];
-// const res = arr.filter1((item, i, arr) => i > 5);
-
-// // node Methods/filter.js
-
+Array.prototype.filter1 = function (callback) {
+  const newArr = [];
+  for (let i = 0; i < this.length; i++) {
+    const element = this[i];
+    if (callback(element, i, this)) {
+      newArr.push(element);
+    }
+  }
+  return newArr;
+};
+// !========== Test ===========
+const arr = [2, 3, 4, 5, 6, 7, 8];
+const res = arr.filter1((item, i, arr) => item > 5);
+console.log(res);
--- Array/ArrayMethods/find.js
+++ Array/ArrayMethods/find.js
@@ -1,15 +1,15 @@
-// buning findIndex() dan farqi 
+// buning findIndex() dan farqi
 // -- i ning o'rniga this[i]
 // -- -1 ning o'rniga undefined qaytaradi
-const  find1 = function (callback) {
+Array.prototype.find1 = function (callback) {
   for (let i = 0; i < this.length; i++) {
     if (callback(this[i], i, this)) {
       return this[i];
     }
   }
   return undefined;
-}
-Array.prototype.find1 = find1;
+};
 //!======= Test =======
 const nums = [5, 12, 8, 130, 44, 140];
-console.log(nums.find1(elm => elm > 13));
+const res = nums.find1((elm) => elm > 6);
+console.log(res);
