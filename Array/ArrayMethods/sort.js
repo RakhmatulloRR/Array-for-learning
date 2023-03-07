7d47693e9d2b52be3fe4070d99a54b47cb95c1f5
RakhmatilloRR <rrr.engineer.94@gmail.com>
2023-03-07 09:50:44 +0500

    Sort method was reviewed

--- Array/ArrayMethods/sort.js
+++ Array/ArrayMethods/sort.js
@@ -1,12 +1,12 @@
 Array.prototype.sort1 = function (callback) {
   for (let i = 1; i < this.length; i++) {
-    let d = 0;
+    let isWorking = false;
     for (let j = 0; j < this.length - 1; j++) {
       if (callback) {
         let a = this[j];
         let b = this[j + 1];
         if (callback(a, b) > 0) {
-          d += 1;
+          isWorking = true;
           let c = this[j];
           this[j] = this[j + 1];
           this[j + 1] = c;
@@ -15,15 +15,15 @@ Array.prototype.sort1 = function (callback) {
         let a = this[j].toString();
         let b = this[j + 1].toString();
         if (a > b) {
-          d += 1;
+          isWorking = true;
           let c = this[j];
           this[j] = this[j + 1];
           this[j + 1] = c;
         }
       }
     }
-    // agar d = 0 bo'lsa jarayon yakunlandi degani
-    if (d === 0) {
+    console.log(isWorking);
+    if (!isWorking) {
       return this;
     }
   }
@@ -34,5 +34,7 @@ Array.prototype.sort1 = function (callback) {
 const nums = [2, 3, 4, 5, 72, 6, 710, 7, 701, 8];
 const texts = ['a', 'e', 'd', 'ad', 'c', 'b', 'ac'];
 
-const result = nums.sort1();
+const result = nums.sort1((a, b) => {
+  return a - b;
+});
 console.log(result);
