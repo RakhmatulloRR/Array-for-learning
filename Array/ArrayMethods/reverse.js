d4ad969fb0da72918eecb543ffa172057ce2b5a2
RakhmatilloRR <rrr.engineer.94@gmail.com>
2023-03-07 09:40:55 +0500

    Sort method was reviewed

--- Array/ArrayMethods/reverse.js
+++ Array/ArrayMethods/reverse.js
@@ -1,17 +1,28 @@
 Array.prototype.reverse1 = function () {
-  let left = 0;                 // 0 1 2
-  let right = this.length - 1;  // 6 5 4
-  while(left < right) {
+  let left = 0; // 0 1 2
+  let right = this.length - 1; // 6 5 4
+  // while(left < right) {
+  //     let temp = this[left];
+  //     this[left] = this[right];
+  //     this[right] = temp;
+  //     left++;
+  //     right--;
+  // }
+  for (let i = 0; i < this.length; i++) {
+    if (left < right) {
       let temp = this[left];
       this[left] = this[right];
       this[right] = temp;
       left++;
       right--;
+    } else {
+      return this;
+    }
   }
-  return this;
 };
 // ! ======= Test =======
 
-const arr = [1, "b", "abc", { name: "Jonh" }, 10 , 7, 6];
+const arr = [1, 'b', 'abc', { name: 'Jonh' }, 10, 7, 6];
+const result = arr.reverse1();
 
-console.log(arr.reverse1()); // [10, { name: 'Jonh' }, 'abc', 'b', 1]
+console.log(result); // [6, 7, 10, { name: 'Jonh' }, 'abc', 'b', 1]
--- Array/ArrayMethods/sort.js
+++ Array/ArrayMethods/sort.js
@@ -1,8 +1,12 @@
-function sort1(callback) {
+Array.prototype.sort1 = function (callback) {
   for (let i = 1; i < this.length; i++) {
+    let d = 0;
     for (let j = 0; j < this.length - 1; j++) {
       if (callback) {
-        if (callback(this[j], this[j+1]) > 0) {
+        let a = this[j];
+        let b = this[j + 1];
+        if (callback(a, b) > 0) {
+          d += 1;
           let c = this[j];
           this[j] = this[j + 1];
           this[j + 1] = c;
@@ -11,21 +15,24 @@ function sort1(callback) {
         let a = this[j].toString();
         let b = this[j + 1].toString();
         if (a > b) {
+          d += 1;
           let c = this[j];
           this[j] = this[j + 1];
           this[j + 1] = c;
         }
       }
     }
+    // agar d = 0 bo'lsa jarayon yakunlandi degani
+    if (d === 0) {
+      return this;
+    }
   }
   return this;
-}
-Array.prototype.sort1 = sort1;
+};
 
 // !======= Test ========
 const nums = [2, 3, 4, 5, 72, 6, 710, 7, 701, 8];
-const texts = ["a", "e", "d", "ad", "c", "b", "ac"];
+const texts = ['a', 'e', 'd', 'ad', 'c', 'b', 'ac'];
 
-const result = nums.sort1((a, b) => { return a - b });
+const result = nums.sort1();
 console.log(result);
-// console.log(texts);
