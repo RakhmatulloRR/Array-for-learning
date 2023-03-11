5e3bec756baff9b141e394017390c73c3f7a1dfa
RakhmatilloRR <rrr.engineer.94@gmail.com>
2023-03-11 07:14:19 +0500

    Strin toLowerCase() method was implemanted

---
+++ String/toLowerCase.js
@@ -0,0 +1,16 @@
+
+String.prototype.toLowerCase1 = function () {
+  upper = '';
+  for (let i = 0; i < this.length; i++) {
+    if (str.charCodeAt(i) > 64 && this.charCodeAt(i) < 91) {
+      upper += String.fromCharCode(this.charCodeAt(i) + 32);
+    } else {
+      upper += this[i];
+    }
+  }
+  return upper;
+};
+
+str = 'Hello World';
+upper = str.toLowerCase1();
+console.log(upper);
