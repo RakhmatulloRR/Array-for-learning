d02c5484eae873e35759700aea2ad4f12aecb233
RakhmatilloRR <rrr.engineer.94@gmail.com>
2023-03-13 11:46:48 +0500

    String indexOf() and lastIndexOf() methods were implemented

--- String/methods/19_lastIndexOf.js
+++ String/methods/19_lastIndexOf.js
@@ -1,29 +1,29 @@
-String.prototype.indexOf1 = function (ch) {
+String.prototype.lastIndexOf1 = function (ch) {
   if (typeof ch === 'undefined') {
     return -1;
   }
   if (ch === '' && typeof ch === 'string') {
-    return 0;
+    return this.length;
   }
-  let j = 0;
+  let j = 1;
   let match = 0;
-  for (let i = 0; i < this.length; i++) {
-    console.log(i, this[i], ch[j]);
-    if (this[i] === ch[j]) {
+  for (let i = 1; i <= this.length; i++) {
+    console.log(i, this[this.length - i], ch[ch.length - j]);
+    if (this[this.length - i] === ch[ch?.length - j]) {
       match++;
       j++;
       if (match === ch.length) {
         console.log('d: ', i, j, match);
-        return i - (ch.length - 1); // this statement is exacuted at the end.
+        return this.length - i; // this statement is exacuted at the end.
       }
       console.log('a: ', i, j, match);
-    } else if (this[i] === ch[0]) {
+    } else if (this[this.length - i] === ch[ch.length - 1]) {
       match = 0;
-      j = 0;
+      j = 1;
       console.log('b: ', i, j, match);
     } else {
       match = 0;
-      j = 0;
+      j = 1;
       console.log('c: ', i, j, match);
     }
   }
@@ -32,6 +32,6 @@ String.prototype.indexOf1 = function (ch) {
 
 //! ====== test ======
 const str = 'Hello World';
-const index = str.indexOf('H');
-const index1 = str.indexOf1('H');
+const index = str.lastIndexOf('Wo');
+const index1 = str.lastIndexOf1('Wo');
 console.log(index, index1);
